bafb835f0ed692eb764dc6f11899891f4b1c269e
kizisoft <kizisoft@gmail.com>
2015-10-31 16:11:02 +0200

    Remove some files

--- public/app/directives/messagesPanel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.directive('messagesPanel', function () {
-    return {
-        restrict: 'AE',
-        templateUrl: 'templates/directives/messagesPanel.html',
-        replace: true
-    };
-});
